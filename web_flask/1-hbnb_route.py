74233e4804198a65c5f4c071280a3b74dd6adf8e
talent <124376251+CourageT1@users.noreply.github.com>
2023-10-22 21:33:31 +0200

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,24 +1,21 @@
-!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
 """
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    return "HBNB"
+    return 'HBNB'
 
 
 if __name__ == '__main__':
