27aa1140fa1b18fce04802ba60750935c8983504
talent <124376251+CourageT1@users.noreply.github.com>
2023-10-22 21:54:36 +0200

    Update 10-hbnb_filters.py

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /hbnb_filters: HBnB HTML filters page.
 """
+Starts a Flask web application.
+"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
-    """Displays the main HBnB filters HTML page."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
+@app.route('/states')
+def states_list():
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('states.html', states=sorted_states)
+
+
+@app.route('/states/<id>')
+def state_cities(id):
+    state = storage.get("State", id)
+    if state:
+        cities = sorted(state.cities, key=lambda city: city.name)
+        return render_template('state.html', state=state, cities=cities)
+    else:
+        return render_template('not_found.html')
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(exception):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
