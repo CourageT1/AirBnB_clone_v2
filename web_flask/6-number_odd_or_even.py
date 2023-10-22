9684ae76f9560fec3887346e949631b372688c09
talent <124376251+CourageT1@users.noreply.github.com>
2023-10-22 21:20:59 +0200

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -22,26 +22,34 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return "C " + text.replace('_', ' ')
 
+
 @app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+
+
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     return "Python " + text.replace('_', ' ')
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_number(n):
     return "{} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', n=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
